04a0fb4b8fbb5d1cd8a866db6a8be2a156da4c6f
Sebola3461 <mixvideostubecontato@gmail.com>
2021-07-06 21:24:50 -0300

    tests

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
-app.listen(3000, () => {
+app.listen(3000, "osumaniaskins.herokuapp.com", () => {
     console.log("Running")
 })
 
